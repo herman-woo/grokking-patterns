2d5188b053542ecfd954b854f9876066aa1d9070
Hermanm <hermanwoo100@gmail.com>
2023-10-18 18:44:39 -0400

    Updated Palindrome

---
+++ JavaScript/TwoPointers-Palindrome.js
@@ -0,0 +1,29 @@
+const jsonData = require('./data/palindrome.json')
+
+function main() {
+    jsonData.words.forEach(word => {
+        let state = "is"
+        if (!isPalindrome(word)) {
+            state = state + " not"
+        }
+        console.log(`${word} ${state} a palindrome`)
+    }
+    )
+}
+
+function isPalindrome(word) {
+    let left = 0;
+    let right = word.length - 1
+    while (left < right) {
+
+        if (word[left] !== word[right]) {
+            return false
+        }
+
+        left++
+        right--
+    }
+    return true
+}
+
+main()
